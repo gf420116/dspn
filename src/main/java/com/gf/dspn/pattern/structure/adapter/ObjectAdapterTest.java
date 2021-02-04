8a295b0a748deade27b988f2fb16620b1abcfdc9
doublej <gfwy0323@gmail.com>
2021-02-04 22:02:21 +0800

    add structure create mode proxy adapter

---
+++ src/main/java/com/gf/dspn/pattern/structure/adapter/ObjectAdapterTest.java
@@ -0,0 +1,25 @@
+package com.gf.dspn.pattern.structure.adapter;
+
+//对象适配器类
+class ObjectAdapter implements Target {
+    private Adaptee adaptee;
+
+    public ObjectAdapter(Adaptee adaptee) {
+        this.adaptee = adaptee;
+    }
+
+    public void request() {
+        adaptee.specificRequest();
+    }
+}
+
+//对象适配器模式
+//客户端代码
+public class ObjectAdapterTest {
+    public static void main(String[] args) {
+        System.out.println("对象适配器模式测试：");
+        Adaptee adaptee = new Adaptee();
+        Target target = new ObjectAdapter(adaptee);
+        target.request();
+    }
+}
---
+++ src/main/java/com/gf/dspn/pattern/structure/adapter/ReadXML.java
@@ -0,0 +1,26 @@
+package com.gf.dspn.pattern.structure.adapter;
+
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.io.*;
+
+class ReadXML {
+    public static Object getObject() {
+        try {
+            DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = dFactory.newDocumentBuilder();
+            Document doc;
+            doc = builder.parse(new File("src/main/java/com/gf/dspn/pattern/structure/adapter/config.xml"));
+            NodeList nl = doc.getElementsByTagName("className");
+            Node classNode = nl.item(0).getFirstChild();
+            String cName = "com.gf.dspn.pattern.structure.adapter." + classNode.getNodeValue();
+            Class<?> c = Class.forName(cName);
+            Object obj = c.newInstance();
+            return obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
