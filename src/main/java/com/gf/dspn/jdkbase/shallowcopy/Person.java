67ee8592e292ee10c3134daaa73910fec868f3ef
doublej <gfwy0323@gmail.com>
2021-12-17 15:19:42 +0800

    浅深拷贝

---
+++ src/main/java/com/gf/dspn/jdkbase/shallowcopy/Person.java
@@ -0,0 +1,43 @@
+package com.gf.dspn.jdkbase.shallowcopy;
+
+/**
+ * 浅拷贝：浅拷贝会在堆上创建一个新的对象（区别于引用拷贝的一点），不过，如果原对象内部的属性是引用类型的话，浅拷贝会直接复制内部对象的引用地址，也就是说拷贝对象和原对象共用同一个内部对象.
+ *
+ * @author : doublej
+ * @Date : 2021-12-13 14:14
+ */
+
+public class Person implements Cloneable {
+    private Address address;
+
+    // 省略构造函数、Getter&Setter方法
+    public Person(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public Person clone() {
+        try {
+            Person person = (Person) super.clone();
+            return person;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person(new Address("武汉"));
+        Person person1Copy = person1.clone();
+        // true
+        System.out.println(person1.getAddress() == person1Copy.getAddress());
+
+    }
+}
